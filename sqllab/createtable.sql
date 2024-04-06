87e2887972bd5f5010d6b5f5c9bb95150a8f60c6
michaela lam <michaela@michaelas-MacBook-Air.local>
2024-04-06 15:07:48 -0500

    created table

--- sqllab/createtable.sql
+++ sqllab/createtable.sql
@@ -1,12 +1,12 @@
 DROP TABLE IF EXISTS earthquakes;
 CREATE TABLE earthquakes (
-  quakedate date,
   quaketime time with time zone,
   latitude real,
   longitude real,
-  quakedepth real,
   mag real,
+  dmin real, 
+  ms real,
   id text,
   place text,
-  quaketype text
+  status text,
 );
