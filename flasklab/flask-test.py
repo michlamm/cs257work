6fe30a77e02093bf0b79ff056ef1a71f8f85a23a
michlamm <165341285+michlamm@users.noreply.github.com>
2024-04-24 10:30:55 -0500

    Update flask-test.py

--- flasklab/flask-test.py
+++ flasklab/flask-test.py
@@ -1,8 +1,8 @@
 import flask
+import psycopg2
 
 app = flask.Flask(__name__)
 
-#
 @app.route('/hello')
 def my_function():
     return "Hello World!"
@@ -20,7 +20,24 @@ def my_color(word1):
 def add(num1, num2):
     answer = int(num1) + int(num2) 
     return str(answer)
+
+@app.route('/pop/<abbrev>')
+def abbrev():
+    conn = psycopg2.connect(
+        host="localhost",
+        port=5432,
+        database="lamm2",
+        user="lamm2",
+        password="corn453smile")
+
+    cur = conn.cursor()
+
+    pop = "select population from population where code = %s"
+        cur.execute(pop)
+        row1 = cur.fetchone()
     
+    return row1
+
 if __name__ == '__main__':
     my_port = 5105
     app.run(host='0.0.0.0', port = my_port) 
