d3ec1fc233b9ae3aef0e29874fadff6d880a6308
michlamm <165341285+michlamm@users.noreply.github.com>
2024-05-15 10:09:10 -0500

    Create spotify.js

---
+++ csslab/static/spotify.js
@@ -0,0 +1,44 @@
+function functionname() {
+the_heading = document.getElementById("hello");
+
+heading_text = the_heading.innerText;
+
+if (heading_text != "Hello World!"){
+the_heading.innerText = "Hello World!"
+}
+else {
+	the_heading.innerText = "Goodbye"
+}}
+
+the_paragraph = document.getElementById("paragraph1");
+
+the_paragraph.onmouseenter = function() { 
+	the_paragraph.style.color = "blue";
+  the_paragraph.style.backgroundColor = "white";
+};
+
+function changeSpotify() {
+  text_input_element = document.getElementById("user-color");
+  new_color = text_input_element.value;
+	the_heading.style.color = new_color;
+}
+
+/* When the user clicks on the button,
+toggle between hiding and showing the dropdown content */
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
