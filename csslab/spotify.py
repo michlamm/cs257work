89f3b0309053898d4f9ea04e4aa043ef0b33dcfe
michlamm <165341285+michlamm@users.noreply.github.com>
2024-05-15 10:11:32 -0500

    Update spotify.py

--- csslab/spotify.py
+++ csslab/spotify.py
@@ -1,6 +1,8 @@
-<link rel="stylesheet" href="/static/spotify.css">
+<head>
+  <link rel="stylesheet" href="/static/spotify.css">
 
-<script src="/static/spotify.js"></script>
+  <script src="/static/spotify.js"></script>
+</head>
 
 <h1 class="my-class" id="hello" onclick="functionname()">Hello World!</h1>
 <p>Color: <input id="user-color" type="text" /> &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;<button onclick="changeSpotify()">change color </button></p>
@@ -9,7 +11,7 @@
   <button onclick="myFunction()" class="dropbtn">Choose a Playlist!</button>
   <div id="myDropdown" class="dropdown-content">
   <a href="https://open.spotify.com/playlist/37i9dQZF1EQqZgBURAEzWH?si=2b44ac9a621e495d">Christian Mix</a></p></a>
-    <p><a href="https://open.spotify.com/playlist/37i9dQZF1E39cUmGkJbWqy?si=0c2a3a5bafb449f6">Daily Mix 1</a></p>
+   <p><a href="https://open.spotify.com/playlist/37i9dQZF1E39cUmGkJbWqy?si=0c2a3a5bafb449f6">Daily Mix 1</a></p>
     <p><a href="https://open.spotify.com/playlist/37i9dQZF1DX0D996ZXujBy?si=0feb1ad5608b4ebb">Taylor Swift</a></p>
   </div>
 </div>
