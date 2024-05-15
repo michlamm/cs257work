3121258f4f11954d0403fa5642e0e5e7d04094a7
michlamm <165341285+michlamm@users.noreply.github.com>
2024-05-15 10:14:48 -0500

    Update spotify.py

--- csslab/spotify.py
+++ csslab/spotify.py
@@ -1,17 +1,14 @@
-<head>
-  <link rel="stylesheet" href="/static/spotify.css">
 
-  <script src="/static/spotify.js"></script>
-</head>
+from flask import Flask
+from flask import render_template
 
-<h1 class="my-class" id="hello" onclick="functionname()">Hello World!</h1>
-<p>Color: <input id="user-color" type="text" /> &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;<button onclick="changeSpotify()">change color </button></p>
+app = Flask(__name__)
 
-<div class="dropdown">
-  <button onclick="myFunction()" class="dropbtn">Choose a Playlist!</button>
-  <div id="myDropdown" class="dropdown-content">
-  <a href="https://open.spotify.com/playlist/37i9dQZF1EQqZgBURAEzWH?si=2b44ac9a621e495d">Christian Mix</a></p></a>
-   <p><a href="https://open.spotify.com/playlist/37i9dQZF1E39cUmGkJbWqy?si=0c2a3a5bafb449f6">Daily Mix 1</a></p>
-    <p><a href="https://open.spotify.com/playlist/37i9dQZF1DX0D996ZXujBy?si=0feb1ad5608b4ebb">Taylor Swift</a></p>
-  </div>
-</div>
+@app.route('/')
+def welcome():
+	return render_template(“spotify.html”)
+
+if __name__ == '__main__':
+    my_port = 5105
+
+    app.run(host='0.0.0.0', port = my_port) 
